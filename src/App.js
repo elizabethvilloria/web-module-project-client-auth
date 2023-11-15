e3ec80b4595eccf7919ead715de3059f03c5326d
elizabethvilloria <lizvilloria@gmail.com>
2023-11-15 08:58:23 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
+const Login = () => 
+  (<h2>Login</h2>);
+
+const FriendList = () => 
+  (<h2>Friends</h2>);
+
+const AddFriend = () => 
+  (<h2>Add Friend</h2>);
 
 function App() {
   return (
     <div className="App">
-      <h2>Client Auth Project</h2>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Navigate to="/" replace />} />
+        <Route path="/friends" element={<FriendList />} />
+        <Route path="/friends/add" element={<AddFriend />} />
+      </Routes>
     </div>
   );
 }
